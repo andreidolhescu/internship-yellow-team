31b866593ecfcba1e5b5ea425b11f75f8ed7ef87
Vasile Maruseac <inter.vasile.maruseac@assist.ro>
2018-08-14 12:11:41 +0300

    Added user and category tables

---
+++ server/migrations/20180814082444-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      FirstName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      LastName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      Email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      UserRole: {
+        defaultValue: false,
+        type: Sequelize.BOOLEAN
+      },
+      Points: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      PathforImage: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
