3824541099abbbefc59bc25d9d8d6c6f9dee4b73
Vasile Maruseac <inter.vasile.maruseac@assist.ro>
2018-08-17 14:21:02 +0300

    Corrected chapterController

--- server/controllers/ChapterController.js
+++ server/controllers/ChapterController.js
@@ -4,17 +4,28 @@ module.exports = {
     // insert chapter into chapter table
     create (req, res) {
         return ChapterModel
+           .create({
+                Title: req.body.Title,
+                Content: req.body.Content,
+                courseId: req.params.courseId
+            })
+           .then(todo => res.status(201).send(todo))
+           .catch(error => res.status(400).send(error));
+    },
+
+    // get all entries from Chapter table for a courseid
+    list (req, res) {
+        return ChapterModel
             .findAll({
             where: {
                 courseId: req.params.courseId
-                }
+                    }
         })
             .then(chapter => {
                 if(chapter == "")
                 {
                     return res.status(404).send({
                     message: 'There are no chapters for this course!',
-
                     });
                 }
                 if (!chapter) {
@@ -24,21 +35,7 @@ module.exports = {
                 }
                 return res.status(200).send(chapter);
               })
-            .catch(error => res.status(400).send(error));
-    },
 
-    // get all entries from Chapter table for a courseid
-    list (req, res) {
-        return ChapterModel
-            .findById(req.params.courseId)
-            .then(course => {
-                if (!course) {
-                  return res.status(404).send({
-                    message: 'Course Not Found',
-                  });
-                }
-                return res.status(200).send(course);
-              })
             .catch(error => res.status(400).send(error));
     },
 
