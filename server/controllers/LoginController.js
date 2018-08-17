37477fe2132d9c8c734806a477c0a40b66b3373f
Lorin <intern.lorin.cucu@assist.ro>
2018-08-17 10:02:52 -0700

    Token Work

--- server/controllers/LoginController.js
+++ server/controllers/LoginController.js
@@ -35,16 +35,23 @@ module.exports =
                     console.log('Aici');
 
                     const payload = {
-                        Mail : userObj.Mail
+                        Mail: userObj.Mail
                     }
 
-                    const token = jwt.sign(payload,settings.SecurityToken,{
-                        expiresIn : 1
+                    const token = jwt.sign(payload, settings.SecurityToken, {
+                        expiresIn: 60
                     });
-                    console.log(token);
-                    return res.status(200).send({
+
+                    return res.json({
+                        success: true,
+                        message: 'Enjoy your token!',
                         token: token,
-                    })
+                    });
+                    //console.log("A modificat tokenul");
+
+                    /*return res.status(200).send({
+                        token: token,
+                    })*/
                 }
                 else {
                     return res.status(404).send({
@@ -52,8 +59,34 @@ module.exports =
                     });
                 }
             }).catch(err => {
-                console.log("Eroare ",err);
+                console.log("\n\nEroare functia 1\n\n", err);
                 return res.status(400).send(err);
+            });
+        },
+        GetToken(req, res, next) {
+            var token = req.body.token || req.query.token || req.headers['x-access-token'];
+            if (token) {
+                jwt.verify(token, settings.SecurityToken, function (err, decoded) {
+                    if (err) {
+                        console.log("\n\nFunctia 2\n\n", err);
+                        console.log("Token - ", token);
+                        return res.json({ success: false, message: 'Failed to authenticate token.' });
+                    } else {
+                        // if everything is good, save to request for use in other routes
+                        req.decoded = decoded;
+                        next();
+                    }
+                });
+            }
+            else {
+                return res.status(404).send({
+                    message: "No token!",
+                });
+            }
+        },
+        ItsValidToken(req, res) {
+            return res.status(200).send({
+                message: "Functia ItsValidToken, Functia 3",
             });
         }
     }
