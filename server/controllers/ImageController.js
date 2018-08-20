adfee7879e33f5f93a751c17e163f95c38b643c9
Vasile Maruseac <inter.vasile.maruseac@assist.ro>
2018-08-20 10:52:24 +0300

    Changed Image Controller

--- server/controllers/ImageController.js
+++ server/controllers/ImageController.js
@@ -4,8 +4,9 @@ module.exports = {
     // insert image into Image table -> TREBUIE FACUT SEPARARE INTRE CURS SI USER
     create: (req, res) => {
         return ImageModel.create({
-            Question: req.body.Question,
-            chapterId: req.params.chapterId
+            path: req.body.path,
+            courseId: req.params.courseId,
+            userId: req.params.userId
         })
         .then(todo => res.status(201).send(todo))
         .catch(error => {
@@ -14,64 +15,47 @@ module.exports = {
         });
     },
 
-    // get all entries from Quiz table
+    // get all entries from Image table
     list (req, res) {
-        return QuizModel
-            .findAll({
-            where: {
-                chapterId: req.params.chapterId
-                }
-        })
-            .then(quiz => {
-                if(quiz == "")
-                {
-                    return res.status(404).send({
-                    message: 'There are not quizzes for this chapter!',
-
-                    });
-                }
-                if (!quiz) {
-                  return res.status(404).send({
-                    message: 'Quiz not found!',
-                  });
-                }
-                return res.status(200).send(quiz);
-              })
+        return ImageModel
+            .all()
+            .then(images => res.status(200).send(images))
             .catch(error => res.status(400).send(error));
     },
 
     // get an entry by id
     getById (req, res) {
-        return QuizModel
-            .findById(req.params.quizId)
-            .then(quiz => {
-                if (!quiz) {
+        return ImageModel
+            .findById(req.params.imageId)
+            .then(image => {
+                if (!image) {
                   return res.status(404).send({
-                    message: 'Quiz Not Found',
+                    message: 'Image Not Found',
                   });
                 }
-                return res.status(200).send(quiz);
+                return res.status(200).send(image);
               })
               .catch(error => res.status(400).send(error));
     },
 
-    // update an entry -> TODO: Only for admins
+    // update an entry
     update (req, res) {
-        return QuizModel
-            .findById(req.params.quizId)
-            .then(quiz => {
-                if (!quiz) {
+        return ImageModel
+            .findById(req.params.imageId)
+            .then(image => {
+                if (!image) {
                     return res.status(404).send({
-                        message: 'Quiz Not Found',
+                        message: 'Image Not Found',
                     });
                 }
                 
-            return quiz
+            return image
                 .update({
-                    Question: req.body.Question,
-                    chapterId: req.params.chapterId
+                    path: req.body.path,
+            		courseId: req.params.courseId,
+            		userId: req.params.userId
                 })
-                .then(() => res.status(200).send(quiz))
+                .then(() => res.status(200).send(image))
                 .catch((error) => res.status(400).send(error));
             
                     
@@ -79,18 +63,18 @@ module.exports = {
             .catch((error) => res.status(400).send(error));
     },
 
-    // delete an entry -> TODO: Only for admins
+    // delete an entry
     destroy (req, res) {
-        return QuizModel
-            .findById(req.params.quizId)
-            .then(quiz => {
-                if (!quiz) {
+        return ImageModel
+            .findById(req.params.imageId)
+            .then(image => {
+                if (!image) {
                     return res.status(404).send({
-                        message: 'Quiz Not Found',
+                        message: 'Image Not Found',
                     });
                 }
 
-                return quiz
+                return image
                     .destroy()
                     .then(() => res.status(200).send())
                     .catch((error) => res.status(400).send(error));
