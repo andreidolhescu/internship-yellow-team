fde6ed3acd47a603c76877f351bf0924aac8e801
Vasile Maruseac <inter.vasile.maruseac@assist.ro>
2018-08-20 15:08:51 +0300

    Modified ImageController to list user images and course image

--- server/routes/index.js
+++ server/routes/index.js
@@ -87,6 +87,10 @@ module.exports = (app) => {
     app.post('/api/user/:userId/image', ImageController.create); // insert an user image
     app.post('/api/category/:categoryId/course/:courseId/image', ImageController.create); // insert a course image
     app.get('/api/image', ImageController.list); // list all images
+    //TODO: List all users images
+    app.get('/api/usr/images', ImageController.userlist);
+    //TODO: List images for courses
+    app.get('/api/curs/images', ImageController.curslist);
     app.get('/api/image/:imageId', ImageController.getById);
     app.put('/api/user/:userId/image/:imageId', ImageController.update);//update a user image
     app.put('/api/category/:categoryId/course/:courseId/image/:imageId', ImageController.update);//update a course image
