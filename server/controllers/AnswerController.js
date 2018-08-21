4190834e4faf8bc41ea5cd2a5866815f65474d17
Lorin <intern.lorin.cucu@assist.ro>
2018-08-20 17:40:46 -0700

    Partial add answer

--- server/controllers/AnswerController.js
+++ server/controllers/AnswerController.js
@@ -1,21 +1,61 @@
 const AnswerModel = require('../models').Answer;
 
+const jwt = require('jsonwebtoken');
+const settings = require('../config/Index');
+const UserModel = require('../models').User;
+
+function GetUserIdByToken(token, callback) {
+    jwt.verify(token, settings.SecurityToken, function (err, decoded) {
+        if (err) {
+            return res.json({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+            getUser(decoded.Mail, function (result) {
+                if (result != "null") {
+                    callback(result.id);
+                }
+                else {
+                    callback(null);
+                }
+            })
+
+
+        }
+    });
+}
+
+const getUser = (mail, res) =>
+    UserModel.findOne({
+        where: {
+            Mail: mail
+        }
+    }).then(userTokens => {
+        if (userTokens != null) {
+            return res(userTokens);
+        }
+        else {
+            return res("null");
+        };
+    });
+
 module.exports = {
     // insert Answer into Answer table
     create: (req, res) => {
-        return AnswerModel.create({
-            quizOptionsId: req.params.quizOptionsId,
-            //TODO UserId: de preluat jwt  
+        GetUserIdByToken(req.headers['token'], function (result) {
+            return AnswerModel.create({
+                quizOptionsId: req.params.quizOptionsId,
+                UserId: result
+                //TODO UserId: de preluat jwt  
+            })
+                .then(todo => res.status(201).send(todo))
+                .catch(error => {
+                    console.log("dssdv", error)
+                    return res.status(400).send(error);
+                });
         })
-        .then(todo => res.status(201).send(todo))
-        .catch(error => {
-        	console.log("dssdv", error)
-        	return res.status(400).send(error);
-        });
     },
 
     // get all entries from Answer table
-    list (req, res) {
+    list(req, res) {
         return AnswerModel
             .all()
             .then(todos => res.status(200).send(todos))
@@ -23,22 +63,22 @@ module.exports = {
     },
 
     // get an entry by id
-    getById (req, res) {
+    getById(req, res) {
         return AnswerModel
             .findById(req.params.answerId)
             .then(answer => {
                 if (!answer) {
-                  return res.status(404).send({
-                    message: 'Answer Not Found',
-                  });
+                    return res.status(404).send({
+                        message: 'Answer Not Found',
+                    });
                 }
                 return res.status(200).send(answer);
-              })
-              .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error));
     },
 
     // delete an entry
-    destroy (req, res) {
+    destroy(req, res) {
         return AnswerModel
             .findById(req.params.answerId)
             .then(answer => {
